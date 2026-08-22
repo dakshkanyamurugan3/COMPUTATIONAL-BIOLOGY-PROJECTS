8836ac33d0f4561726d4745aa7ba13a050924caa
dakshkanyamurugan3 <dakshkanya.m@gmail.com>
2026-08-22 19:19:35 +0530

    Add files via upload
    DNA Nucleotide Counter: A Python script that counts the number of A, T, G, and C nucleotides in a given DNA sequence.

---
+++ dna_nucleotidecounterpython.py
@@ -0,0 +1,6 @@
+dna_sequence = input("Enter a DNA sequence: ").upper()
+
+print("A:", dna_sequence.count("A"))
+print("T:", dna_sequence.count("T"))
+print("G:", dna_sequence.count("G"))
+print("C:", dna_sequence.count("C"))
